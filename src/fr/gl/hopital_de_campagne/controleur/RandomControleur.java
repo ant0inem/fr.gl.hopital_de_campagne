f982af6d734f809dac308737f7541ff5609beb33
ant0inem <Antoine@MacBook-Pro-de-Antoine.local>
2015-03-10 16:43:49 +0100

    UTF8

--- src/fr/gl/hopital_de_campagne/controleur/RandomControleur.java
+++ src/fr/gl/hopital_de_campagne/controleur/RandomControleur.java
@@ -1,5 +1,9 @@
 package fr.gl.hopital_de_campagne.controleur;
+<<<<<<< Updated upstream
 // Cette classe ne sert vraiment à rien. Normal, Bruno l'a créée :-)
+=======
+// Cette classe ne sert vraiment à  rien. Normal, Bruno l'a créée :-)
+>>>>>>> Stashed changes
 public class RandomControleur {
 
 	
