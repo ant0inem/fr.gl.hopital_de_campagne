43814cdbaf35519ca4cd0de79de5e3d9a354574d
Bruno Lédée <bruno.ledee@gmail.com>
2015-03-10 17:02:13 +0100

    on en a marre des conneries
    MERCI

--- src/fr/gl/hopital_de_campagne/controleur/RandomControleur.java
+++ src/fr/gl/hopital_de_campagne/controleur/RandomControleur.java
@@ -1,9 +1,5 @@
 package fr.gl.hopital_de_campagne.controleur;
-<<<<<<< Updated upstream
 // Cette classe ne sert vraiment à rien. Normal, Bruno l'a créée :-)
-=======
-// Cette classe ne sert vraiment à  rien. Normal, Bruno l'a créée :-)
->>>>>>> Stashed changes
 public class RandomControleur {
 
 	
