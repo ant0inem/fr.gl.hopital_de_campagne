037b57cf2e768454579e09077ab95ca50c351c18
ant0inem <Antoine@MacBook-Pro-de-Antoine.local>
2015-03-24 16:12:14 +0100

    Ajout import persistance

--- src/fr/gl/hopital_de_campagne/dao/Container_containsDao.java
+++ src/fr/gl/hopital_de_campagne/dao/Container_containsDao.java
@@ -1,5 +1,14 @@
 package fr.gl.hopital_de_campagne.dao;
 
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Container_containsDao {
 
 }
