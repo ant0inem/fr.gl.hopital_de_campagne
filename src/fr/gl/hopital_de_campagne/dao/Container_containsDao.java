7c60d67a05d9beba338ab57ee67bff6b562e3203
ant0inem <Antoine@MacBook-Pro-de-Antoine.local>
2015-03-25 15:29:02 +0100

    Container_containsDao

---
+++ src/fr/gl/hopital_de_campagne/dao/Container_containsDao.java
@@ -0,0 +1,33 @@
+package fr.gl.hopital_de_campagne.dao;
+// RESTE Container_Secteur_idSecteur à écrire.
+import java.util.List;
+
+import javax.persistence.*;
+
+@Entity
+public class Container_containsDao {
+
+	@OneToOne
+	@JoinColumn(name = "idContainer")
+	ContainerDao container;
+
+	public void setContainer(ContainerDao c) {
+		container = c;
+	}
+
+	public ContainerDao getContainer() {
+		return container;
+	}
+	
+	@OneToMany
+	@JoinColumn(name = "idEquipement")
+	EquipementDao equipement;
+
+	public void setEquipement(EquipementDao e) {
+		equipement = e;
+	}
+
+	public EquipementDao getEquipement() {
+		return equipement;
+	}
+}
