2392a7470d09c15cd022438aae647237528d7c78
brunoledee <bruno.ledee@gmail.com>
2015-04-09 22:19:49 +0200

    Création Class Dao - la classe que doit coder Antoine

---
+++ src/fr/gl/hopital_de_campagne/dao/Dao.java
@@ -0,0 +1,15 @@
+package fr.gl.hopital_de_campagne.dao;
+
+import java.util.List;
+
+public class Dao {
+	
+	/**
+	 * 
+	 * @return the set of all the equipement saved in the data base
+	 */
+	public List<EquipementDao> getAllEquipementDao() {
+		return null;
+	}
+
+}
