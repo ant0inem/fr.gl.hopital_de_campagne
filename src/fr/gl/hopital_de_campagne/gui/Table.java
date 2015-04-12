aa2db1207bec3e7d905445fac6d2ea3390da4d3a
brunoledee <bruno.ledee@gmail.com>
2015-04-12 19:29:33 +0000

    Ajout de commentaires à la classe Table

--- src/fr/gl/hopital_de_campagne/gui/Table.java
+++ src/fr/gl/hopital_de_campagne/gui/Table.java
@@ -10,10 +10,24 @@ public abstract class Table {
 		
 	}
 	
+	/**
+	 * Returns the JPanel which contains the table.
+	 * @return the JPanel
+	 */
 	public abstract JPanel getTable();
 	
+	/**
+	 * Returns the index of the first selected row, 
+	 * -1 if no row is selected.
+	 * @return the index of the first selected row
+	 */
 	public abstract int getIndexOfSelectedRow();
 	
+	/**
+	 * 
+	 * @param rowIndex index of a row
+	 * @return the list of object which represent the specify row
+	 */
 	public abstract List<Object> getValuesOfRow(int rowIndex);
 
 }
